f9bb8dc9ffad91966d829a499601372e8899a9cc
richard-dennett <richard.dennett@medhurst-it.com>
2018-04-28 12:02:41 +0100

    returned vlaue

--- Functions.py
+++ Functions.py
@@ -1,7 +1,8 @@
 def SplitFunction(string)
     Split_String_Var =(string)
     Split_String_Var1,Split_String_Var2 = Split_String_Var.split(" ")
-    if Split_String_Var2 eq "1":
-        return True
-    else
-        return False
+    return Split_String_Var2
+    #if Split_String_Var2 eq "1":
+    #    return True
+    #else
+    #    return False
