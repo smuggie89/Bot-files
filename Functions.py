d3b3b92b062b16bf80eff19729ba7ae47586e70e
richard-dennett <richard.dennett@medhurst-it.com>
2018-04-28 11:48:41 +0100

    Functions file
    Functions for global use of bots

---
+++ Functions.py
@@ -0,0 +1,7 @@
+def SplitFunction(string)
+    Split_String_Var =(string)
+    Split_String_Var1,Split_String_Var2 = Split_String_Var.split(" ")
+    if Split_String_Var2 eq "1":
+        return True
+    else
+        return False
