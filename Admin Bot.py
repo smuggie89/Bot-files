1a73c30afc003ec592d3eb1b15a8e718fe5aef3f
smuggie89 <38769192+smuggie89@users.noreply.github.com>
2018-04-28 03:19:51 +0100

    amend

--- Admin Bot.py
+++ Admin Bot.py
@@ -23,7 +23,7 @@ async def clear(ctx, number):
     async for x in client.logs_from(ctx.message.channel, limit = number):
         mgs.append(x)
     await client.delete_messages(mgs)
-    #await client.say('Messages deleted')
+    await client.say('Messages deleted')
 
 @client.command(pass_context = True)
 async def getbans(ctx):
@@ -31,56 +31,56 @@ async def getbans(ctx):
     x = '\n'.join([y.name for y in x])
     embed = discord.Embed(title = "List of Banned Members", description = x, color = 0xFFFFF)
     return await client.say(embed = embed)
-    
-@client.command(pass_context = True)
-async def ban(ctx, *, member : discord.Member = None):
-    if not ctx.message.author.server_permissions.administrator:
-        return
 
-    if not member:
-        return await client.say(ctx.message.author.mention + "Specify a user to ban!")
-    try:
-        await client.ban(member)
-    except Exception as e:
-        if 'Privilege is too low' in str(e):
-            return await client.say(":x: Privilege too low!")
-
-    embed = discord.Embed(description = "**%s** has been banned!"%member.name, color = 0xFF0000)
-    return await client.say(embed = embed)
-
-@client.command(pass_context = True)
-async def kick(ctx, *, member : discord.Member = None):
-    if not ctx.message.author.server_permissions.administrator:
-        return
-
-    if not member:
-        return await client.say(ctx.message.author.mention + "Specify a user to kick!")
-    try:
-        await client.kick(member)
-    except Exception as e:
-        if 'Privilege is too low' in str(e):
-            return await client.say(":x: Privilege too low!")
-
-    embed = discord.Embed(description = "**%s** has been kicked!"%member.name, color = 0xFF0000)
-    return await client.say(embed = embed)
-
-@client.event
-async def on_message(message):
-        if message.content.upper().startswith('!RESTART CHEF'):
-            os.system('taskkill /f /im py.exe /FI "WINDOWTITLE eq Chef"')
-            time.sleep(10)
-            os.system('"Chef Bot.py"')
-        if message.content.upper().startswith('!RESTART SURVIVOR'):
-            os.system('taskkill /f /im py.exe /FI "WINDOWTITLE eq Survivor"')
-            time.sleep(10)
-            os.system('"Survivor Bot.py"')
-        if message.content.upper().startswith('!RESTART JOHN'):
-            os.system('taskkill /f /im py.exe /FI "WINDOWTITLE eq John"')
-            time.sleep(10)
-            os.system('"John Bot.py"')
-        if message.content.upper().startswith('!RESTART SMUG'):
-            os.system('taskkill /f /im py.exe /FI "WINDOWTITLE eq Smug"')
-            time.sleep(10)
-            os.system('"Smug Test Bot.py"')
+#@client.command(pass_context = True)
+#async def ban(ctx, *, member : discord.Member = None):
+#    if not ctx.message.author.server_permissions.administrator:
+#        return
+#
+#    if not member:
+#        return await client.say(ctx.message.author.mention + "Specify a user to ban!")
+#    try:
+#        await client.ban(member)
+#    except Exception as e:
+#        if 'Privilege is too low' in str(e):
+#            return await client.say(":x: Privilege too low!")
+#
+#    embed = discord.Embed(description = "**%s** has been banned!"%member.name, color = 0xFF0000)
+#    return await client.say(embed = embed)
+#
+#@client.command(pass_context = True)
+#async def kick(ctx, *, member : discord.Member = None):
+#    if not ctx.message.author.server_permissions.administrator:
+#        return
+#
+#    if not member:
+#        return await client.say(ctx.message.author.mention + "Specify a user to kick!")
+#    try:
+#        await client.kick(member)
+#    except Exception as e:
+#        if 'Privilege is too low' in str(e):
+#            return await client.say(":x: Privilege too low!")
+#
+#    embed = discord.Embed(description = "**%s** has been kicked!"%member.name, color = 0xFF0000)
+#    return await client.say(embed = embed)
+#
+#@client.event
+#async def on_message(message):
+#        if message.content.upper().startswith('!RESTART CHEF'):
+#            os.system('taskkill /f /im py.exe /FI "WINDOWTITLE eq Chef"')
+#            time.sleep(10)
+#            os.system('"Chef Bot.py"')
+#        if message.content.upper().startswith('!RESTART SURVIVOR'):
+#            os.system('taskkill /f /im py.exe /FI "WINDOWTITLE eq Survivor"')
+#            time.sleep(10)
+#            os.system('"Survivor Bot.py"')
+#        if message.content.upper().startswith('!RESTART JOHN'):
+#            os.system('taskkill /f /im py.exe /FI "WINDOWTITLE eq John"')
+#            time.sleep(10)
+#            os.system('"John Bot.py"')
+#        if message.content.upper().startswith('!RESTART SMUG'):
+#            os.system('taskkill /f /im py.exe /FI "WINDOWTITLE eq Smug"')
+#            time.sleep(10)
+##            os.system('"Smug Test Bot.py"')
 
 client.run(BotsKey.AB)
