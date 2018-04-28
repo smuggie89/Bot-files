4b75815d7eadf19a0bb49ed3b9962ef2dd4fbddf
richard-dennett <richard.dennett@medhurst-it.com>
2018-04-28 12:17:02 +0100

    cache

--- ChefBot.py
+++ ChefBot.py
@@ -32,7 +32,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     def e_w_k_f(kibblename, kibblenumber):
-        if message.content.upper().endswith(kibblename) or SplitFunction(message.content.upper().equals(kibblenumber):
+        if message.content.upper().endswith(kibblename) or message.content.upper().endswith(kibblenumber):
             return True
 
     def em_k_f(eggname, eggrecipe, eggtaming):
