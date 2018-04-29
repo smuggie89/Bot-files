bd7c28dd9390dd004b000684d0a1e6b71fd2a287
smuggie89 <38769192+smuggie89@users.noreply.github.com>
2018-04-30 00:20:29 +0100

    amend

--- AdminBot.py
+++ AdminBot.py
@@ -45,30 +45,6 @@ async def on_member_join(member):
 
 @client.event
 async def on_message(message):
-        if message.content.upper().startswith('!ADMIN'):
-            #if admin == True:
-            member = message.author
-            User = message.author
-            await client.send_message(member, "As requested %s, the admin codes you require: \n \t - !admin - Request DM of latest Admin commands (in #general channel only) \n \t - !clear # - Clear messages in channel" % (User.mention))
-        else:
-            await client.send_message(message.channel, "You are not authorised to do that")
-        await client.process_commands(message)
-
-#Help
-@client.event
-async def on_message(message):
-    if message.content.upper().startswith('!HELP'):
-         for server in client.servers:
-             for channel in server.channels:
-                 if channel.name == 'general':
-                    if "438702024918302734" in [role.id for role in message.author.roles]:
-                        embed = discord.Embed(title="Commands", description="Ah, so i see you need some help! Let me provide you with some queastions you can ask", color=0x27408B)
-                        embed.add_field(name="\u200b \t !getbans", value="\u200b \t gives you a list of banned players", inline=False)
-                        await client.send_message(message.author, embed=embed)
-    await client.process_commands(message)
-
-@client.event
-async def on_message(message):
         if message.content.upper().startswith('!RESTART CHEF'):
             os.system('taskkill /f /im py.exe /FI "WINDOWTITLE eq Chef"')
             time.sleep(10)
@@ -85,6 +61,25 @@ async def on_message(message):
             os.system('taskkill /f /im py.exe /FI "WINDOWTITLE eq Smug"')
             time.sleep(10)
             subprocess.Popen(['py.exe', 'SmugTestBot.py'], creationflags = subprocess.CREATE_NEW_CONSOLE)
+
+        if message.content.upper().startswith('!HELP'):
+             for server in client.servers:
+                 for channel in server.channels:
+                     if channel.name == 'general':
+                        if "438702024918302734" in [role.id for role in message.author.roles]:
+                            embed = discord.Embed(title="Commands", description="Ah, so i see you need some help! Let me provide you with some queastions you can ask", color=0x27408B)
+                            embed.add_field(name="\u200b \t !getbans", value="\u200b \t gives you a list of banned players", inline=False)
+                            await client.send_message(message.author, embed=embed)
+
+        if message.content.upper().startswith('!ADMIN'):
+            member = message.author
+            User = message.author
+            server = member.server
+            for user in server.members:
+                if user.server_permissions.administrator:
+                    await client.send_message(member, "As requested %s, the admin codes you require: \n \t - !admin - Request DM of latest Admin commands (in #general channel only) \n \t - !clear # - Clear messages in channel" % (User.mention))
+            else:
+                await client.send_message(message.channel, "You are not authorised to do that")
         await client.process_commands(message)
 
 #@client.command(pass_context = True)
