73d22bea5e8f91e0cdcc65fef31a5c730c796055
smuggie89 <38769192+smuggie89@users.noreply.github.com>
2018-04-29 14:50:20 +0100

    amend

--- ChefBot.py
+++ ChefBot.py
@@ -32,7 +32,6 @@ async def on_ready():
 @client.event
 async def on_message(message):
     def e_w_k_f(kibblename, kibblenumber):
-        #if message.content.upper().endswith(kibblename) or message.content.upper().endswith(kibblenumber):
          if message.content.upper().endswith(kibblename) or (Functions.SplitFunction(message.content) is kibblenumber):
              return True
 
@@ -79,7 +78,6 @@ async def on_message(message):
                     embed.add_field(name="\u200b \t !kibble <kibble name>", value="\u200b \t provides recipe and details of sepcified kibble", inline=False)
                     embed.add_field(name="\u200b \t !food", value="\u200b \t gives a list of all available food recipes", inline=False)
                     embed.add_field(name="\u200b \t !food <food name>", value="\u200b \t provides details of specified food", inline=False)
-                    #embed.add_field(name=" ", value="\t !kibble - gives you a list of all available kibble recipes \n \t !kibble <kibble name> - provides recipe and details of sepcified kibble \n \t !food - gives a list of all available food recipes \n \t !food <food name> - provides details of specified food", inline=False)
                     await client.send_message(channel, embed=embed)
 #Kibble Recipes
     if message.content.upper().startswith('!KIBBLE'):
@@ -329,7 +327,6 @@ async def on_message(message):
         elif message.content.endswith(''):
             embed = discord.Embed(title="Unsure which kibble recipe you require well let me help choose from:", description="\u200b \n \t 1. Allosaurus Egg \n \t 2. Ankylo Egg \n \t 3. Araneo Egg \n \t 4. Archaeopteryx Egg \n \t 5. Argentavis Egg \n \t 6. Bronto Egg \n \t 7. Baryonyx Egg \n \t 8. Bronto Egg \n \t 9. Carno Egg \n \t 10. Compy Egg \n \t 11. Dilo Egg \n \t 12. Dimetrodon Egg \n \t 13. Dimorph Egg \n \t 14. Diplo Egg \n \t 15. Dodo Egg \n \t 16. Featherlight Egg \n \t 17. Gallimimus Egg \n \t 18. Glowtail Egg \n \t 19. Ichthyornis Egg \n \t 20. Iguanodon Egg \n \t 21. Kairuku Egg \n \t 22. Kaprosuchus Egg \n \t 23. Kentro Egg \n \t 24. Lystrosaurus Egg \n \t 25. Mantis Egg \n \t 26. Megalania Egg \n \t 27. Megalosaurus Egg \n \t 28. Microraptor Egg \n \t 29. Morellatops Egg \n \t 30. Moschops Egg \n \t 31. Moth Egg \n \t 32. Oviraptor Egg \n \t 33. Pachy Egg \n \t 34. Pachyrhino Egg \n \t 35. Parasaur Egg \n \t 36. Pegomastax Egg \n \t 37. Pelagornis Egg \n \t 38. Pteranodon Egg \n \t 39. Pulmonoscorpious Egg \n \t 40. Quetzal Egg \n \t 41. Raptor Egg \n \t 42. Rock Drake Egg \n \t 43. Sarco Egg \n \t 44. Spino Egg \n \t 45. Stego Egg \n \t 46. Tapejara Egg \n \t 47. Terror Bird Egg \n \t 48. Therizino Egg \n \t 49. Thorny Dragon Egg \n \t 50. Titanoboa Egg \n \t 51. Trike Egg \n \t 52. Troodon Egg \n \t 53. Turtle Egg \n \t 54.Vulture Egg", color=0x00BFFF)
             await client.send_message(message.channel, embed=embed)
-#                    await client.send_message(message.channel, "Unsure which kibble recipe you require well let me help choose from: \n \t Kibble Ankylo Egg \n \t Kibble Baryonyx Egg \n \t Kibble Carbonemys Egg \n \t Kibble Dilo Egg \n \t Kibble Dimetrodon Egg \n \t Kibble Dimorph Egg \n \t Kibble Dodo Egg \n \t Kibble Featherlight Egg \n \t Kibble Gallimimus Egg \n \t Kibble Glowtail Egg \n \t Kibble Lystrosaurus Egg \n \t Kibble Mantis Egg \n \t Kibble Morellatops Egg \n \t Kibble Pachy Egg \n \t Kibble Parasaur Egg \n \t Kibble Pteranodon Egg \n \t Kibble Raptor Egg \n \t Kibble Rock Drake Egg \n \t Kibble Spino Egg \n \t Kibble Stego Egg \n \t Kibble Terror Bird Egg \n \t Kibble Thorny Dragon Egg \n \t Kibble Trike Egg \n \t Kibble Vulture Egg")
 
 
 client.run(BotsKey.CB)
--- JohnBot.py
+++ JohnBot.py
@@ -29,21 +29,28 @@ async def on_ready():
     print("ID: {}".format(client.user.id))
     await client.change_presence(game=discord.Game(name='Lego Jurassic World'))
 
-@Client.event
+@client.event
 async def on_message(message):
-    contents = message.content.split(" ")
-    for word in contents:
-        if word.upper() in chat_filter:
-            if not message.author.id in bypass_list:
-                try:
-                    await client.delete_message(message)
-                    await client.send_message(message.channel, "**Hey!** You're not allowed to use that word here!")
-                except discord.errors.NotFound:
-                    return
+    #contents = message.content.split(" ")
+    #for word in contents:
+    #    if word.upper() in chat_filter:
+    #        if not message.author.id in bypass_list:
+    #            try:
+    ##                await client.delete_message(message)
+    #                await client.send_message(message.channel, "**Hey!** You're not allowed to use that word here!")
+    #            except discord.errors.NotFound:
+    #                return
+#Help
+    if message.content.upper().startswith('!HELP'):
+         for server in client.servers:
+             for channel in server.channels:
+                 if channel.name == 'dinos':
+                    embed = discord.Embed(title="Commands for #dino channel", description="Ah, so i see you need some help! Let me provide you with some queastions you can ask", color=0x27408B)
+                    await client.send_message(channel, embed=embed)
 
-    if message.content.startswith('!clear'):
-            tmp = await client.send_message(message.channel, 'Clearing messages...')
-            async for msg in client.logs_from(message.channel):
-                await client.delete_message(msg)
+#    if message.content.startswith('!clear'):
+#            tmp = await client.send_message(message.channel, 'Clearing messages...')
+#            async for msg in client.logs_from(message.channel):
+#                await client.delete_message(msg)
 
 client.run(BotsKey.JB)
