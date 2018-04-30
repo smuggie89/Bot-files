201fac13a6684086b711e11225a804d5a1931dbe
richard-dennett <richard.dennett@medhurst-it.com>
2018-05-01 00:00:12 +0100

    updated

--- Functions.py
+++ Functions.py
@@ -1,8 +1,3 @@
-def SplitFunction(string):
-    Split_String_Var =(string)
-    Split_String_Var1,Split_String_Var2 = Split_String_Var.split(" ")
-    return Split_String_Var2
-    #if Split_String_Var2 is compare:
-    #    return True
-    #else
-    #    return False
+def SplitFunction(string, dil):
+    Split_String_Var =(string).split(dil)
+    return Split_String_Var
